2149ccb68d0c8a081041dc8be27719c2aef01e92
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-07-23 16:48:25 +0100

    compound interest basic tests

--- compound_interest/compound_interest.py
+++ compound_interest/compound_interest.py
@@ -1,1 +1,11 @@
 class CompoundInterest:
+
+    def __init__(self, principal, interest_rate, time_period):
+        self.principal = principal
+        self.interest_rate = interest_rate/100.00
+        self.time_period = time_period
+        self.number_invervals = 12
+    
+    def compound_interest(self):  
+        return self.principal * (((1 + (self.interest_rate/self.number_invervals)) ** (self.number_invervals * self.time_period)))
+    
--- compound_interest/compound_interest_test.py
+++ compound_interest/compound_interest_test.py
@@ -6,14 +6,30 @@ class CompoundInterestTest(unittest.TestCase):
 
     # Tests
     # Should return 732.81 given 100 principal, 10 percent, 20 years           
-git 
+    def test_principle_of_100_10_20_returns_732(self):
+        compound_interest = CompoundInterest(100,10,20)
+        self.assertAlmostEqual(732.81,compound_interest.compound_interest(),2)
+    
+
     # Should return 181.94 given 100 principal, 6 percent, 10 years
-    
+    def test_principle_of_100_6_10_returns_182(self):
+        compound_interest = CompoundInterest(100,6,10)
+        self.assertAlmostEqual(181.94,compound_interest.compound_interest(),2)
+
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
-    
+    def test_principle_of_100000_5_8_returns_149058(self):
+        compound_interest = CompoundInterest(100000,5,8)
+        self.assertAlmostEqual(149058.55,compound_interest.compound_interest(),2)
+
     # Should return 0.00 given 0 principal, 10 percent, 1 year
-    
+    def test_principle_of_0_10_1_returns_0(self):
+        compound_interest = CompoundInterest(0,10,1)
+        self.assertAlmostEqual(0.00,compound_interest.compound_interest(),2)
+
     # Should return 100.00 given 100 principal, 0 percent, 10 years
+    def test_principle_of_100_0_10_returns_100(self):
+        compound_interest = CompoundInterest(100,0,10)
+        self.assertAlmostEqual(100.00,compound_interest.compound_interest(),2)
 
 
     # Extention tests
